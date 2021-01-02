2b97b264b0bf2604470562386296bfb9fd645146
Alex Wright <AlexParadiseWright+git@gmail.com>
2021-01-02 01:47:27 +0100

    Consolidated Part9 as it was bugging me

--- p9.go
+++ p9.go
@@ -5,9 +5,10 @@ import (
     "io/ioutil"
     "strconv"
     "strings"
+    "math"
 )
 
-func main() {
+func part1() {
     var lines []int
     var preamble int = 25
 
@@ -52,4 +53,47 @@ func main() {
         delete(cache, last[0])
         last = append(last[1:], item)
     }
+}
+
+func part2() {
+    var lines []int
+    var magicN int = 1398413738 //127
+
+    dat, _ := ioutil.ReadFile("in.9")
+
+    for _, item := range strings.Split(string(dat), "\n") {
+        item, _ := strconv.Atoi(item)
+		lines = append(lines, item)
+	}
+
+    var l int = 0
+    var u int = 1
+    var sum int =  lines[l] + lines[u]
+
+    for (sum != magicN) {
+        if (sum < magicN) {
+            u += 1
+            sum += lines[u]
+        } else if (sum > magicN) {
+            sum -= lines[l]
+            l += 1
+        }
+    }
+
+    var min float64 = float64(magicN)
+    var max float64 = 0
+
+    for i := l; i < u; i++ {
+        min = math.Min(min, float64(lines[i]))
+        max = math.Max(max, float64(lines[i]))
+    }
+
+    fmt.Println(min)
+    fmt.Println(max)
+    fmt.Println(int(min + max))
+}
+
+func main() {
+    part1()
+    part2()
 }
